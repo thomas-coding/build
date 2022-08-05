f587562c3787762053ecb205b3a566297ddb0e2b
Jinping Wu <jinping.wu@verisilicon.com>
2022-08-05 18:18:46 +0800

    pi pico add freertos build

---
+++ raspberrypi_pico/build_freertos_smp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# shell folder
+shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
+
+export PATH="/home/cn1396/.toolchain/gcc-arm-none-eabi-10.3-2021.07/bin/:$PATH"
+export PICO_SDK_PATH=${shell_folder}/pico-sdk
+export FREERTOS_KERNEL_PATH=${shell_folder}/FreeRTOS/FreeRTOS/Source
+
+# change to SMP branch kernel
+cd "${shell_folder}/FreeRTOS/FreeRTOS/Source" || exit
+git checkout origin/smp
+
+# which demo to compile
+examples_dir=${shell_folder}/FreeRTOS/FreeRTOS/Demo/CORTEX_M0+_RP2040
+if [[ ! -d ${examples_dir} ]]; then
+    cp -rf ${shell_folder}/FreeRTOS/FreeRTOS/Demo/ThirdParty/Community-Supported/CORTEX_M0+_RP2040 \
+        ${shell_folder}/FreeRTOS/FreeRTOS/Demo/CORTEX_M0+_RP2040
+fi
+
+# output
+elf=${shell_folder}/FreeRTOS/FreeRTOS/Demo/CORTEX_M0+_RP2040/build/OnEitherCore/on_core_one.elf
+
+# build makefile
+cd "${examples_dir}" || exit
+rm -rf build
+cmake -B build -DCMAKE_BUILD_TYPE=Debug
+
+# build
+cd "${examples_dir}" || exit
+make -C build
+
+# dump
+cd "${shell_folder}" || exit
+rm -f freertos.asm
+arm-none-eabi-objdump -xD ${elf} > freertos.asm
