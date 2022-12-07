fe6151dd7dcd44fd1ace78a284ab1dcaec5eefb8
Jinping Wu <jinping.wu@verisilicon.com>
2022-12-07 15:14:01 +0800

    update bear build for support mbedtls

--- bear/build.sh
+++ bear/build.sh
@@ -4,12 +4,19 @@
 shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
 export PATH="/home/cn1396/.toolchain/gcc-arm-none-eabi-10.3-2021.07/bin:$PATH"
 
+baremetal_dir=${shell_folder}/bear/baremetal
+mbedtls_dir=${shell_folder}/mbedtls
 
+# build mbedtls
+cd "${mbedtls_dir}" || exit
+rm -rf build
+cmake -B build -DENABLE_TESTING=Off -DENABLE_PROGRAMS=Off -DTEST_CPP=Off -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=${baremetal_dir}/cmake/cortex_m33.cmake
+cmake --build ./build
 
-platform_dir=${shell_folder}/bear/baremetal
-
-# build
-cd "${platform_dir}" || exit
+# build baremetal
+cd "${baremetal_dir}" || exit
 rm -rf build
 cmake -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=cmake/cortex_m33.cmake
 cmake --build ./build
+
+
--- bear/rungdb.sh
+++ bear/rungdb.sh
@@ -2,16 +2,25 @@
 
 export PATH="/home/cn1396/.toolchain/gcc-arm-none-eabi-10.3-2021.07/bin:$PATH"
 
+# GDB port
+#port=2331
+port=3333
+
 # shell folder
 shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
 
 demo_dir=${shell_folder}/bear/baremetal
 demo_elf=${demo_dir}/build/bear.elf
 
+pc_value=0x202007c0
 
 # gdb
 arm-none-eabi-gdb \
--ex 'target remote localhost:2331' \
+-ex 'target extended-remote localhost:3333' \
 -ex "load ${demo_elf}" \
 -ex "add-symbol-file ${demo_elf}" \
+-ex "si" \
+-ex "monitor reset halt" \
+-ex "si" \
+-ex "set \$pc=${pc_value}" \
 -q
