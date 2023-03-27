87204e7337e6320bea313e6735d1dbf12a916d89
Jinping Wu <jinping.wu@verisilicon.com>
2023-03-27 17:03:35 +0800

    vp: support sd raw block load

--- virtual_platform/arm_arch64/build_sd.sh
+++ virtual_platform/arm_arch64/build_sd.sh
@@ -3,15 +3,28 @@
 # shell folder
 shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
 
-#image_dir=${shell_folder}/out/images
-sleep 1
+rawblkload=1
 
 echo "shell_folder:${shell_folder}"
 
 # Create 32M sd card
 rm -f ${shell_folder}/sd.disk
-dd if=/dev/zero of=${shell_folder}/sd.disk bs=1M count=32
+dd if=/dev/zero of=${shell_folder}/sd.disk bs=1M count=256
 
 # Burn fip.bin to sd card, offset 20K
 dd if=${shell_folder}/arm-trusted-firmware/build/a55/release/fip.bin of=sd.disk conv=fsync,notrunc bs=512 seek=40
 sync
+
+# rawblkload, put dtb and Image to SD card, uboot through mmc read, read it from sd card to ddr
+if [[ ${rawblkload} = 1 ]]; then
+    # Burn a55.dtb to sd card, offset 16MB
+    # Refer: u-boot/include/configs/a55.h
+    dd if=${shell_folder}/linux/arch/arm64/boot/dts/virtual_platform/a55.dtb of=sd.disk conv=fsync,notrunc bs=512 seek=32768
+    sync
+
+    # Burn Image to sd card, offset 18MB
+    # Refer: u-boot/include/configs/a55.h
+    dd if=${shell_folder}/linux/arch/arm64/boot/Image of=sd.disk conv=fsync,notrunc bs=512 seek=36864
+    sync
+fi
+
--- virtual_platform/arm_arch64/runqemu.sh
+++ virtual_platform/arm_arch64/runqemu.sh
@@ -18,9 +18,6 @@ fi
 
 qemu_option+=" -machine thomas-a55"
 qemu_option+=" -kernel ${shell_folder}/arm-trusted-firmware/build/a55/release/bl1/bl1.elf"
-#qemu_option+=" -device loader,file=${shell_folder}/arm-trusted-firmware/build/a55/release/fip.bin,addr=0x21000000"
-qemu_option+=" -device loader,file=${shell_folder}/linux/arch/arm64/boot/Image,addr=0x35000000"
-qemu_option+=" -device loader,file=${shell_folder}/linux/arch/arm64/boot/dts/virtual_platform/a55.dtb,addr=0x36000000"
 qemu_option+=" -device loader,file=${shell_folder}/buildroot/output/images/rootfs.tar,addr=0x37000000"
 #qemu_option+=" -d guest_errors"
 qemu_option+=" -nographic"
@@ -30,6 +27,8 @@ if [[ ${boot_from_sd} = 1 ]]; then
     qemu_option+=" -drive if=sd,file=${shell_folder}/sd.disk,format=raw"
 else
     qemu_option+=" -device loader,file=${shell_folder}/arm-trusted-firmware/build/a55/release/fip.bin,addr=0x21000000"
+    qemu_option+=" -device loader,file=${shell_folder}/linux/arch/arm64/boot/Image,addr=0x35000000"
+    qemu_option+=" -device loader,file=${shell_folder}/linux/arch/arm64/boot/dts/virtual_platform/a55.dtb,addr=0x36000000"
 fi
 
 # Change to develop qemu
