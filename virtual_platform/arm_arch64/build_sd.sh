4578d2260a3df621d1a035772e18a7219177ebb1
Jinping Wu <jinping.wu@verisilicon.com>
2023-03-30 10:06:20 +0800

    vp: sd make fat filesystem

--- virtual_platform/arm_arch64/build_sd.sh
+++ virtual_platform/arm_arch64/build_sd.sh
@@ -3,13 +3,15 @@
 # shell folder
 shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
 
-rawblkload=1
+rawblkload=0
+
+sd_name=sd.disk
 
 echo "shell_folder:${shell_folder}"
 
 # Create 32M sd card
-rm -f ${shell_folder}/sd.disk
-dd if=/dev/zero of=${shell_folder}/sd.disk bs=1M count=256
+rm -f ${shell_folder}/${sd_name}
+dd if=/dev/zero of=${shell_folder}/sd.disk bs=1M count=1024
 
 # Burn fip.bin to sd card, offset 20K
 dd if=${shell_folder}/arm-trusted-firmware/build/a55/release/fip.bin of=sd.disk conv=fsync,notrunc bs=512 seek=40
@@ -26,5 +28,56 @@ if [[ ${rawblkload} = 1 ]]; then
     # Refer: u-boot/include/configs/a55.h
     dd if=${shell_folder}/linux/arch/arm64/boot/Image of=sd.disk conv=fsync,notrunc bs=512 seek=36864
     sync
+
+    exit
 fi
 
+# emulate virtual block device
+sudo losetup /dev/loop100 ${sd_name}
+#losetup -a
+
+#
+# p1: offset 50M, size 450M
+# 102400 = 102400 * 512 = 50M
+# p2: offset 500M, size 500M
+
+sudo fdisk /dev/loop100 <<EOF1
+n
+p
+1
+102400
++450M
+n
+p
+2
+1024000
+
+t
+1
+c
+p
+w
+
+EOF1
+
+# probe partition, get /dev/loop100p1 and /dev/loop100p2
+sudo partprobe /dev/loop100
+
+# make file system
+sudo mkfs.vfat -I /dev/loop100p1
+sudo mkfs.ext4 /dev/loop100p2 -F
+
+# copy image to sd
+echo "----- copy kernel image and dtb"
+sudo rm -rf ${shell_folder}/tmp
+mkdir ${shell_folder}/tmp
+sudo mount -t vfat /dev/loop100p1 ${shell_folder}/tmp
+sudo cp -f -r ${shell_folder}/linux/arch/arm64/boot/dts/virtual_platform/a55.dtb ${shell_folder}/tmp
+sudo cp -f -r ${shell_folder}/linux/arch/arm64/boot/Image ${shell_folder}/tmp
+sleep 1
+sudo umount ${shell_folder}/tmp
+echo "----- install image done"
+
+
+# remove block device
+sudo losetup -d /dev/loop100
