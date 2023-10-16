6f94cb11f36f35b4ea3237219a75a10a1a57b593
Jinping Wu <jinping.wu@verisilicon.com>
2023-10-16 11:21:04 +0800

    add riscv64 baremetal

---
+++ virtual_platform/riscv64_bm/rungdb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# shell folder
+shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
+
+
+#export PATH="/opt/riscv/bin:$PATH"
+export PATH="/home/cn1396/.toolchain/riscv/riscv64-elf-ubuntu-20.04-gcc-nightly-2023.10.12-nightly/bin:$PATH"
+
+bm_dir=baremetal/baremetal-riscv64
+cd ${bm_dir}
+# gdb
+#riscv64-unknown-linux-gnu-gdb \
+riscv64-unknown-elf-gdb \
+-ex 'target remote localhost:1234' \
+-ex "add-symbol-file ${shell_folder}/${bm_dir}/output/target.elf" \
+-q
+ 
---
+++ virtual_platform/riscv64_bm/runqemu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Shell folder
+shell_folder=$(cd "$(dirname "$0")" || exit;pwd)
+
+bm_dir=baremetal/baremetal-riscv64
+
+qemu_option=
+if [[ $1  = "--gdb" ]]; then
+    qemu_option+=" -s -S"
+    echo "enable gdb, please run script './rungdb', and enter c "
+else
+    echo "not use gdb, just run"
+fi
+
+qemu_option+=" -machine thomas-riscv64 -bios none -monitor null -semihosting"
+qemu_option+=" --semihosting-config enable=on,target=native"
+qemu_option+=" -kernel ${shell_folder}/${bm_dir}/output/target.elf"
+qemu_option+=" -serial stdio -nographic"
+qemu_option+=" -monitor telnet:127.0.0.1:65530,server,nowait"
+#qemu_option+=" -d guest_errors"
+#qemu_option+=" -smp 2"
+
+# Run qemu
+#qemu-system-riscv64 ${qemu_option}
+qemu/build/riscv64-softmmu/qemu-system-riscv64 ${qemu_option}
+
